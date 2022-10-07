3aa28e6ebdb52575321c10f92f13d6c533a9ef31
Jeffrey King <jking@hey.com>
2022-10-07 19:57:06 -0400

    minor styling fixes on landing page

--- src/components/ByCategory.svelte
+++ src/components/ByCategory.svelte
@@ -5,7 +5,7 @@
 	export let alt: string;
 </script>
 
-<div class="flex-1 text-center">
+<div class="text-center md:w-[50%] sm:max-w-[100%] m-auto mb-8">
 	<a href={link}>
 		<img class="rounded-full w-[200px] m-auto mb-4 border-8" src={imageSrc} {alt} />
 		<h3 class="text-2xl text-[#E9CAE5]">{text}</h3>
