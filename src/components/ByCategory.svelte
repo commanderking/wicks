28b27829742f48eaac899a28413046ee4ebf14f6
Jeffrey King <jking@hey.com>
2022-10-07 20:01:55 -0400

    use provided widths

--- src/components/ByCategory.svelte
+++ src/components/ByCategory.svelte
@@ -5,7 +5,7 @@
 	export let alt: string;
 </script>
 
-<div class="text-center md:w-[50%] sm:max-w-[100%] m-auto mb-8">
+<div class="text-center md:w-2/4 sm:max-w-screen m-auto mb-8">
 	<a href={link}>
 		<img class="rounded-full w-[200px] m-auto mb-4 border-8" src={imageSrc} {alt} />
 		<h3 class="text-2xl text-[#E9CAE5]">{text}</h3>
